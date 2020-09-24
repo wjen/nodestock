cc01fb11ff50b1ababc4500d1dbbe50ede8dd6f8
Wen Jen <wjen1214@gmail.com>
2020-09-23 21:39:49 -0700

    use fetch instead

--- index.js
+++ index.js
@@ -6,26 +6,28 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 require('dotenv').config()
+const fetch = require('node-fetch');
 
 
 // use body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
 
-function call_api(finishedAPI, ticker) {
-    request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=' + process.env.API_KEY, {json: true}, (err, res, body) => {
-        if (err) { return console.log(err);}
-        if (res.statusCode === 200) {
-            finishedAPI(body);
-        }
-    });  
-}
+// function call_api(finishedAPI, ticker) {
+//     request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=' + process.env.API_KEY, {json: true}, (err, res, body) => {
+//         if (err) { return console.log(err);}
+//         if (res.statusCode === 200) {
+//             finishedAPI(body);
+//         }
+//     });  
+// }
 
+function call_api(ticker) {
+    return fetch('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=' + process.env.API_KEY)
+    .then(response => response.json())
+    .then(json => json)
+}
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-
-
-// pk_41bfeb9afa414009afb3bdcc8f9900ad
-
 
 // Set handlebar index GET routes
 app.get('/', function (req, res) {
@@ -37,9 +39,12 @@ app.get('/', function (req, res) {
 // Set handlebar index POST routes
 app.post('/', function (req, res) {
     ticker = req.body.stock_ticker;
-    call_api(function (doneAPI) {
-        res.render('home', {stock: doneAPI, ticker: ticker});
-    }, ticker);
+    if (ticker) {
+        call_api(ticker)
+        .then(response => res.render('home', {stock: response, ticker: ticker}))
+    } else {
+        res.render('home')
+    }
 });
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
